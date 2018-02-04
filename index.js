bf37fec1afdc647138581a367808c403722fa307
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-04 00:56:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  kittens = kittens.push(name)
-  return kittens
+  var newArray = kittens.push(name)
+  return newArray
 }
