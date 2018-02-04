331ebffa9d96d36e5d2125e30e49bc4594a234d8
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-04 01:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,3 +27,8 @@ function prependKitten(name){
   var newArray = [name, ...kittens]
   return newArray
 }
+
+function removeLastKitten(){
+  var newArray = kittens.slice(0, kittens.Length, -1)
+  return newArray
+}
